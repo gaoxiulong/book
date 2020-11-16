8bbf99e875597df9a3928f2b25a9f27b385de6dd
NING MEI <gaoxiulong139@163.com>
2020-11-16 11:59:31 +0800

    update sql

--- sql/t_order_item.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `t_order_item` VALUES (27, '葵花宝典2', 1, 99.00, 99.00, '15953116248181');
-INSERT INTO `t_order_item` VALUES (28, '葵花宝典', 1, 99.00, 99.00, '15953116248181');
-INSERT INTO `t_order_item` VALUES (29, '时间简史11', 1, 30.00, 30.00, '15953116248181');
-INSERT INTO `t_order_item` VALUES (30, '怎样拐跑别人的媳妇', 1, 68.00, 68.00, '159531172234619');
-INSERT INTO `t_order_item` VALUES (31, '木虚肉盖饭', 1, 16.00, 16.00, '159531172234619');
-INSERT INTO `t_order_item` VALUES (32, '时间简史11', 5, 30.00, 150.00, '15953129474161');
-INSERT INTO `t_order_item` VALUES (33, 'java从入门到放弃', 1, 80.00, 80.00, '15953129474161');
--- sql/t_user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `t_user` VALUES (1, 'admin', 'admin', 'admin@atguigu.com');
-INSERT INTO `t_user` VALUES (19, 'gxl139', 'gxl139', 'gxl139@qq.com');
